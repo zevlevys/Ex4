bf6a20000fb2a29a9c9da787358709b06a92cb6a
roey <roey8999@gmail.com>
2019-06-05 21:48:11 +0300

    Started Testing on FlowExampleSimulation

--- MemoryConstants.h
+++ MemoryConstants.h
@@ -1,3 +1,40 @@
+//#pragma once
+//
+//#include <climits>
+//#include <stdint.h>
+//
+//// word
+//typedef int word_t;
+//
+//#define WORD_WIDTH (sizeof(word_t) * CHAR_BIT)
+//
+//// number of bits in the offset,
+//#define OFFSET_WIDTH 4
+//
+//// page/frame size in words
+//// in this implementation this is also the number of entries in a table
+//#define PAGE_SIZE (1LL << OFFSET_WIDTH)
+//
+//
+//// number of bits in a physical address
+//#define PHYSICAL_ADDRESS_WIDTH 10
+//// RAM size in words
+//#define RAM_SIZE (1LL << PHYSICAL_ADDRESS_WIDTH)
+//
+//// number of bits in a virtual address
+//#define VIRTUAL_ADDRESS_WIDTH 20
+//// virtual memory size in words
+//#define VIRTUAL_MEMORY_SIZE (1LL << VIRTUAL_ADDRESS_WIDTH)
+//
+//// number of frames in the RAM
+//#define NUM_FRAMES (RAM_SIZE / PAGE_SIZE)
+//
+//// number of pages in the virtual memory
+//#define NUM_PAGES (VIRTUAL_MEMORY_SIZE / PAGE_SIZE)
+//
+//#define CEIL(VARIABLE) ( (VARIABLE - (int)VARIABLE)==0 ? (int)VARIABLE : (int)VARIABLE+1 )
+//#define TABLES_DEPTH CEIL((((VIRTUAL_ADDRESS_WIDTH - OFFSET_WIDTH) / (double)OFFSET_WIDTH)))
+
 #pragma once
 
 #include <climits>
@@ -9,21 +46,21 @@ typedef int word_t;
 #define WORD_WIDTH (sizeof(word_t) * CHAR_BIT)
 
 // number of bits in the offset,
-#define OFFSET_WIDTH 4
+#define OFFSET_WIDTH 1
 // page/frame size in words
 // in this implementation this is also the number of entries in a table
-#define PAGE_SIZE (1LL << OFFSET_WIDTH)
+#define PAGE_SIZE 2
 
 
 // number of bits in a physical address
-#define PHYSICAL_ADDRESS_WIDTH 10
+#define PHYSICAL_ADDRESS_WIDTH 4
 // RAM size in words
-#define RAM_SIZE (1LL << PHYSICAL_ADDRESS_WIDTH)
+#define RAM_SIZE 16
 
 // number of bits in a virtual address
-#define VIRTUAL_ADDRESS_WIDTH 20
+#define VIRTUAL_ADDRESS_WIDTH 5
 // virtual memory size in words
-#define VIRTUAL_MEMORY_SIZE (1LL << VIRTUAL_ADDRESS_WIDTH)
+#define VIRTUAL_MEMORY_SIZE 32
 
 // number of frames in the RAM
 #define NUM_FRAMES (RAM_SIZE / PAGE_SIZE)
--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,28 +1,18 @@
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
 #include "math.h"
-
-typedef struct{
+#include <iostream>
+#include <vector>
+typedef struct {
     uint64_t first;
     uint64_t second;
 } uint_pair;
-typedef struct{
+
+typedef struct {
     uint64_t first;
     uint64_t second;
     uint64_t third;
 } uint_triple;
-
-
-
-int traverse(uint64_t virtualAddress);
-
-uint64_t getPageNumber(uint64_t address);
-
-void clearTable(uint64_t frameIndex) {
-    for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
-        PMwrite(frameIndex * PAGE_SIZE + i, 0);
-    }
-}
 
 
 /**
@@ -31,12 +21,44 @@ void clearTable(uint64_t frameIndex) {
  * @param level
  * @return
  */
-uint64_t addressSubstr(uint64_t virtualAddress, int level) {
-    return (((1LL << OFFSET_WIDTH) - 1) & (virtualAddress >> (level * OFFSET_WIDTH)));
+//todo:erase this declaration
+void _printRAM();
+
+uint64_t addressSubstr(uint64_t virtualAddress, int level);
+
+int traverse(uint64_t virtualAddress);
+
+uint64_t getPageNumber(uint64_t address);
+
+uint64_t concatInts(uint64_t first, uint64_t second, uint64_t width);
+
+uint64_t findUsableFrame(uint64_t frameIndex, uint64_t *maxFrame, int depth, uint64_t prevPage);
+
+uint64_t findUsableFrameWrapper(uint64_t prevPage);
+
+uint_triple chooseEvicted(uint64_t pageToSwapIn, uint64_t frameIndex, int depth, int page_number);
+
+uint64_t cycDistance(uint64_t page, uint64_t page_to_swap_in);
+
+
+uint_pair chooseEvictedWrapper(uint64_t _pageToSwap);
+
+uint64_t chooseFrame(uint64_t pageToSwap, uint64_t _prevPage);
+
+////////////////////////////////////
+
+void clearTable(uint64_t frameIndex) {
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
+        PMwrite(frameIndex * PAGE_SIZE + i, 0);
+    }
 }
 
-uint64_t concatInts(uint64_t first, uint64_t second, uint64_t width){
-    return  (first << width) | second;
+uint64_t addressSubstr(uint64_t virtualAddress, int level) {
+    return (virtualAddress >> (level * OFFSET_WIDTH)) % PAGE_SIZE;
+}
+
+uint64_t concatInts(uint64_t first, uint64_t second, uint64_t width) {
+    return (first << width) | second;
 }
 
 
@@ -44,9 +66,6 @@ void VMinitialize() {
     clearTable(0);
 }
 
-uint64_t getXthBatch(uint64_t address, uint64_t batchNumber) {
-    return (address >> batchNumber * OFFSET_WIDTH) % PAGE_SIZE;
-}
 
 uint64_t getPageNumber(uint64_t address) {
     return address >> OFFSET_WIDTH;
@@ -57,18 +76,22 @@ int traverse(uint64_t virtualAddress) {
     uint64_t address = 0;
     uint64_t old_address;
     for (int i = TABLES_DEPTH; i >= 0; i--) {
+//        std::cout << "in traverse, i = " << i << std::endl;
         old_address = address;
-        uint64_t sub = getXthBatch(virtualAddress, i);
+        uint64_t sub = addressSubstr(virtualAddress, i);
         PMread(address * PAGE_SIZE + sub, (word_t *) &address);
         if (address == 0) {
-            uint64_t frameIndex = getAvailableFrame(...); //todo: dont eveict stuff we are using
+//            std::cout << "sub = " << sub << std::endl;
+            uint64_t frameIndex = chooseFrame(pageNumber, old_address); //todo: dont eveict stuff we are using
+//            std::cout << "frameIndex = " << frameIndex << std::endl;
             if (i > 0) { //write zeros in frameIndex if its a table
                 clearTable(frameIndex);
             } else { //restore page from memory if its an actual page
                 PMrestore(frameIndex, pageNumber);
             }
-            PMread(old_address * PAGE_SIZE + sub, (word_t *) frameIndex);
+            PMwrite(old_address * PAGE_SIZE + sub, (word_t) frameIndex);
             address = frameIndex;
+            _printRAM();
         }
     }
     return address;
@@ -76,71 +99,68 @@ int traverse(uint64_t virtualAddress) {
 
 
 //todo: not finished- only looks for empty
-uint64_t findUsableFrame(uint64_t frameIndex, uint64_t* maxFrame, int depth) {
+uint64_t findUsableFrame(uint64_t frameIndex, uint64_t *maxFrame, int depth, uint64_t prevPage) {
     //look for empty table with DFS
-    if (depth >= TABLES_DEPTH){
+    if (depth >= TABLES_DEPTH) {
         return 0;
     }
     bool emptyFrame = true;
-    for (uint64_t i = 0; i < PAGE_SIZE; i++){
+    for (uint64_t i = 0; i < PAGE_SIZE; i++) {
         word_t nextTable;
-        PMread(frameIndex + i, &nextTable);
-        if ((uint64_t)nextTable != 0) {
-            if ((uint64_t)nextTable > *maxFrame){
-                *maxFrame = (uint64_t)nextTable;
+        PMread(frameIndex * PAGE_SIZE + i, &nextTable);
+        if ((uint64_t) nextTable != 0) {
+            if ((uint64_t) nextTable > *maxFrame) {
+                *maxFrame = (uint64_t) nextTable;
             }
             emptyFrame = false;
-            uint64_t childReturn = findUsableFrame(nextTable, maxFrame, depth + 1);
-            if(childReturn != 0){ //found an empty frame
+            if(nextTable == prevPage){
+                continue;
+            }
+            uint64_t childReturn = findUsableFrame(nextTable, maxFrame, depth + 1, prevPage);
+            if (childReturn != 0) { //found an empty frame
                 return childReturn;
             }
         }
     }
-    if (emptyFrame){
+    if (emptyFrame) {
         return frameIndex;
     }
     return 0;
 }
 
-uint64_t findUsableFrameWrapper(){
+uint64_t findUsableFrameWrapper(uint64_t prevPage) {
     uint64_t maxFrameFound = 0;
-    uint64_t ret = findUsableFrame(0, &maxFrameFound, 0);
-    if (ret == 0 && maxFrameFound + 1 < NUM_FRAMES){
+    uint64_t ret = findUsableFrame(0, &maxFrameFound, 0, prevPage);
+    if (ret == 0 && maxFrameFound + 1 < NUM_FRAMES) {
         return maxFrameFound + 1;
     }
     return ret; //may be zero
 }
 
 
-//int isTableEmpty(int pageIndex) {
-//    for (int j = 0; j < PAGE_SIZE; j++){
-//        if
-//    }
-//}
-
-
-uint64_t cycDistance(uint64_t page, uint64_t page_to_swap_in){
+uint64_t cycDistance(uint64_t page, uint64_t page_to_swap_in) {
     return fmin(NUM_PAGES - labs(page_to_swap_in - page), labs(page_to_swap_in - page));
 }
 
 
-uint_triple chooseEvicted(uint64_t pageToSwapIn, uint64_t frameIndex, int depth, int page_number){
+uint_triple chooseEvicted(uint64_t pageToSwapIn, uint64_t frameIndex, int depth, int page_number) {
     //reached leaf
-    if (depth >= TABLES_DEPTH){
+    if (depth >= TABLES_DEPTH) {
         uint_triple triple;
         triple.first = page_number;
         triple.second = cycDistance(page_number, pageToSwapIn);
         triple.third = frameIndex;
         return triple;
     }
-    uint64_t maxCycDistance= 0;
-    for (uint64_t j = 0; j < PAGE_SIZE; j++){
+    uint64_t maxCycDistance = 0;
+    for (uint64_t j = 0; j < PAGE_SIZE; j++) {
         word_t nextTable;
         PMread(frameIndex + j, &nextTable);
-        if ((uint64_t)nextTable != 0) {
+        if ((uint64_t) nextTable != 0) {
             //todo: check this calculation of the page offset concat
             uint_triple ret;
-            ret =  chooseEvicted(pageToSwapIn, nextTable, depth+1, concatInts(j, page_number, OFFSET_WIDTH * (depth+1)));
+            ret = chooseEvicted(pageToSwapIn, nextTable, depth + 1,
+                                concatInts(j, page_number, OFFSET_WIDTH * (depth + 1)));
             maxCycDistance = fmaxl(ret.second, maxCycDistance);
         }
     }
@@ -152,7 +172,7 @@ uint_triple chooseEvicted(uint64_t pageToSwapIn, uint64_t frameIndex, int depth,
 }
 
 
-uint_pair chooseEvictedWrapper(uint64_t _pageToSwap){
+uint_pair chooseEvictedWrapper(uint64_t _pageToSwap) {
     uint_triple evict_ret = chooseEvicted(_pageToSwap, 0, 0, 0);
     uint_pair final;
     final.first = evict_ret.first;
@@ -161,9 +181,9 @@ uint_pair chooseEvictedWrapper(uint64_t _pageToSwap){
 }
 
 
-uint64_t chooseFrame(uint64_t pageToSwap, uint64_t){
-    uint64_t usableFrame = findUsableFrameWrapper();
-    if (usableFrame != 0){
+uint64_t chooseFrame(uint64_t pageToSwap, uint64_t _prevPage) {
+    uint64_t usableFrame = findUsableFrameWrapper(_prevPage);
+    if (usableFrame != 0) {
         return usableFrame;
     }
     uint_pair evictionCandidate = chooseEvictedWrapper(pageToSwap);
@@ -173,17 +193,20 @@ uint64_t chooseFrame(uint64_t pageToSwap, uint64_t){
 }
 
 
-int VMread(uint64_t virtualAddress, word_t* value) {
-    //traverse tree TREE_DEPTH times, each time do:
-        //translate part of the offset: PMread(addr*page_size + x, addr)
-        //if(addr == 0) then
-        //Find an unused frame or evict a page from some frame. Suppose this frame number is f
-        //Clear it if it is table
-        //PMwrite(0+x, f)
-        //addr = f
-    //PMRead(addr * page_size + x, value)
+int VMwrite(uint64_t virtualAddress, word_t value) {
+    uint64_t frame_address = traverse(virtualAddress);
+//    std::cout << "in VMwrite" << std::endl;
+    PMwrite(frame_address, value);
     return 1;
 }
+
+int VMread(uint64_t virtualAddress, word_t *value) {
+    uint64_t frame_address = traverse(virtualAddress);
+    PMread(frame_address, value);
+    return 1;
+}
+
+
 //        word_t addr = 0;
 //        for (int i = 0; i < TABLES_DEPTH; i++) {
 //            PMread(addr + 5, &addr);
@@ -218,15 +241,72 @@ int VMread(uint64_t virtualAddress, word_t* value) {
 //addr = f
 //PMRead(addr * page_size + x, value)
 
+//todo:erase this whole part and remove io, vector includes
+//taken from flowexamplesimulation
 
-int VMwrite(uint64_t virtualAddress, word_t value) {
-    uint64_t frame_address = traverse(virtualAddress);
-    PMwrite(frame_address, value);
-    return 1;
+static bool printType = false; // You may change this to True if you want.
+std::vector<word_t> _getRam()
+{
+    std::vector<word_t> RAM(RAM_SIZE);
+    word_t tempWord;
+    for (uint64_t i = 0; i < NUM_FRAMES; i++)
+    {
+        for (uint64_t j = 0; j < PAGE_SIZE; j++)
+        {
+            PMread(i * PAGE_SIZE + j, &tempWord);
+            RAM[i * PAGE_SIZE + j] = tempWord;
+        }
+    }
+    return RAM;
 }
 
-int VMread(uint64_t virtualAddress, word_t *value) {
-    uint64_t frame_address = traverse(virtualAddress);
-    PMread(frame_address, value);
-    return 1;
+
+/**
+ * print the current state of the pysical memory. feel free to use this function is Virtual Memory for debuging.
+ */
+void _printRAM()
+{
+    std::cout << "---------------------" << '\n';
+    std::cout << "Physical Memory:" << '\n';
+    std::vector<word_t> RAM = _getRam();
+
+    if (printType)
+    {
+        for (uint64_t i = 0; i < NUM_FRAMES; i++)
+        {
+            std::cout << "frame " << i << ":\n";
+            for (uint64_t j = 0; j < PAGE_SIZE; j++)
+            {
+                std::cout << "(" << j << ") " << RAM[i * PAGE_SIZE + j] << "\n";
+            }
+            std::cout << "-----------" << "\n";
+        }
+    } else
+    {
+
+        std::cout << "FRAME INDICES -\t";
+        for (uint64_t i = 0; i < NUM_FRAMES; i++)
+        {
+            std::cout << "F" << i << ": (";
+            for (uint64_t j = 0; j < PAGE_SIZE - 1; j++)
+            {
+                std::cout << j << ",\t";
+            }
+            std::cout << PAGE_SIZE - 1 << ")\t";
+        }
+        std::cout << '\n';
+        std::cout << "DATA -\t\t\t";
+        for (uint64_t i = 0; i < NUM_FRAMES; i++)
+        {
+            std::cout << "     ";
+            for (uint64_t j = 0; j < PAGE_SIZE - 1; j++)
+            {
+                std::cout << RAM[i * PAGE_SIZE + j] << ",\t";
+            }
+            std::cout << RAM[i * PAGE_SIZE + PAGE_SIZE - 1] << " \t";
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << "---------------------" << '\n';
 }
